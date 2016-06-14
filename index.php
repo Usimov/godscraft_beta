766d506e2fa0e14d9159350998aaca8f1a25f429
21vek <git@github.com>
2016-06-14 08:00:27 +0300

    11

--- index.php
+++ index.php
@@ -7,9 +7,10 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
 //mb_regex_encoding('UTF-8');
 //session_start();
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
 <head>
 <link rel="stylesheet" type="text/css" href="TWstyle.css">
 <title>TW</title>
@@ -20,6 +21,7 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
 
 <body>
 <h1>Я люблю Котика!</h1>
+<p>Это мой самый любимый котик)))</p>
 </body>
 
 </html>

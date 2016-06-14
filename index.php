c34677b5090aed23e556d98d4385fd614639ed3f
21vek <git@github.com>
2016-06-14 07:50:43 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian');
+//mb_internal_encoding('UTF-8');
+//mb_http_output('UTF-8');
+//mb_http_input('UTF-8');
+//mb_regex_encoding('UTF-8');
+//session_start();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="TWstyle.css">
+<title>TW</title>
+<meta name="description" content="Title, URL, H1-h4 writer">
+<meta name="keywords" content="Titele writer">
+<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+</head>
+
+<body>
+<h1>Я люблю Котика!</h1>
+</body>
+
+</html>

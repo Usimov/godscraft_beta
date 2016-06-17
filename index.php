d93d835fab89a814d1cebd22cbb294fb74a3d26e
Usimov <ilovevvp@tut.by>
2016-06-17 09:52:25 +0300

    Im master

--- index.php
+++ index.php
@@ -13,16 +13,47 @@ setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian')
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="TWstyle.css">
-<title>Godscraft</title>
-<meta name="description" content="Title, URL, H1-h4 writer">
-<meta name="keywords" content="Titele writer">
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+<title>GodsCraft</title>
+<meta name="description" content="GodsCraft">
+<meta name="keywords" content="godscraft">
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+<script type="text/javascript">
+    function SendRequest(){
+            $.ajax({
+                type: "POST",
+                url: "my_handler.php",
+                data: "&data_1="+$('#data_1').val()+"&data_2="+$('#data_2').val(),
+                success: function(response){
+                $('#response').html(response);
+                }
+            });
+    };
+</script>
 </head>
 
 <body>
 <h1>Я люблю Котика!</h1>
 <p>Это мой самый любимый котик)))</p>
 <p>Самый-присамый любимый)</p>
+
+<table>
+    <tr>
+        <th>Запрос</th>
+        <th>Ответ сервера</th>
+        </tr>
+    <tr>
+        <td>
+            <label>Переменная 1: <input type="text" size="10" id="data_1" /></label><br />
+            <label>Переменная 2: <input type="text" size="10" id="data_2" /></label><br />
+            <button onclick="SendRequest();">Послать запрос</button>
+            </td>
+        <td>
+            <div id="response"></div>
+            </td>
+        </tr>
+    </table>
+
+
 </body>
 
 </html>
